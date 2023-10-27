3f296cd6d7d6a3ade3a7fb47c071d6141bff7e5a
RoksolianaS <135868532+RoksolianaS@users.noreply.github.com>
2023-10-26 23:06:00 -0400

    hw7-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const imagesEl = createGalleryImg(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", imagesEl);
+
+function createGalleryImg(item) {
+    return galleryItems
+    .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+</div>`;
+    })
+    .join("");
+}
+
+const onContainerClick = (element) => {
+    element.preventDefault();
+
+const source = element.target.dataset.source;
+
+    if (!source) return;
+const instance = basicLightbox.create(`
+    <img src="${source}"width="800" height="600">`);
+
+instance.show();
+};
+galleryContainer.addEventListener("click", onContainerClick);
